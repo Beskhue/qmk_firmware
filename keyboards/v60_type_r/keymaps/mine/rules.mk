11e6142c08051bda365a2e1aa889c965c32cb359
Thomas Churchman <thomas@kepow.org>
2019-11-07 20:53:03 +0100

    Add custom keymap for v60 type r.

---
+++ keyboards/v60_type_r/keymaps/mine/rules.mk
@@ -0,0 +1,3 @@
+NKRO_ENABLE = yes           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = no        # Enable the RGB Underglow
